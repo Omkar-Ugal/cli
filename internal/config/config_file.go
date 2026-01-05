dc2e9fedd163d7c4164aa5630886dee024c329fb
Justin Chadwell <justin@unikraft.com>
2026-01-05 16:45:23 +0000

    refactor: Simplify config dir function
    The go standard library has functionality to pretty much do exactly this.
    
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- internal/config/config_file.go
+++ internal/config/config_file.go
@@ -8,71 +8,18 @@ package config
 import (
 	"os"
 	"path/filepath"
-	"runtime"
 )
 
 const (
-	UNIKRAFT_CONFIG_DIR = "UNIKRAFT_CONFIG_DIR"
-	XDG_CONFIG_HOME     = "XDG_CONFIG_HOME"
-	XDG_STATE_HOME      = "XDG_STATE_HOME"
-	XDG_DATA_HOME       = "XDG_DATA_HOME"
-	APP_DATA            = "AppData"
-	LOCAL_APP_DATA      = "LocalAppData"
+	UnikraftConfigDirEnv = "UNIKRAFT_CONFIG_DIR"
 )
 
-// Config path precedence
-// 1. UNIKRAFT_CONFIG_DIR
-// 2. XDG_CONFIG_HOME
-// 3. AppData (windows only)
-// 4. HOME
 func ConfigDir() string {
-	var path string
-	if a := os.Getenv(UNIKRAFT_CONFIG_DIR); a != "" {
-		path = a
-	} else if b := os.Getenv(XDG_CONFIG_HOME); b != "" {
-		path = filepath.Join(b, "unikraft")
-	} else if c := os.Getenv(APP_DATA); runtime.GOOS == "windows" && c != "" {
-		path = filepath.Join(c, "unikraft")
-	} else {
-		d, _ := os.UserHomeDir()
-		path = filepath.Join(d, ".config", "unikraft")
+	if path := os.Getenv(UnikraftConfigDirEnv); path != "" {
+		return path
 	}
-
-	return path
-}
-
-// State path precedence
-// 1. XDG_STATE_HOME
-// 2. LocalAppData (windows only)
-// 3. HOME
-func StateDir() string {
-	var path string
-	if a := os.Getenv(XDG_STATE_HOME); a != "" {
-		path = filepath.Join(a, "unikraft")
-	} else if b := os.Getenv(LOCAL_APP_DATA); runtime.GOOS == "windows" && b != "" {
-		path = filepath.Join(b, "unikraft")
-	} else {
-		c, _ := os.UserHomeDir()
-		path = filepath.Join(c, ".local", "state", "unikraft")
+	if path, err := os.UserConfigDir(); err == nil {
+		return filepath.Join(path, "unikraft")
 	}
-
-	return path
-}
-
-// Data path precedence
-// 1. XDG_DATA_HOME
-// 2. LocalAppData (windows only)
-// 3. HOME
-func DataDir() string {
-	var path string
-	if a := os.Getenv(XDG_DATA_HOME); a != "" {
-		path = filepath.Join(a, "unikraft")
-	} else if b := os.Getenv(LOCAL_APP_DATA); runtime.GOOS == "windows" && b != "" {
-		path = filepath.Join(b, "unikraft")
-	} else {
-		c, _ := os.UserHomeDir()
-		path = filepath.Join(c, ".local", "share", "unikraft")
-	}
-
-	return path
+	return ""
 }
