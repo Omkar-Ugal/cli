214885c790748abab60a8e25723f51ff38b0213e
Justin Chadwell <justin@unikraft.com>
2026-01-23 12:37:29 +0000

    feat: Support auto complete in instance subcommands
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- internal/cmd/instances.go
+++ internal/cmd/instances.go
@@ -610,7 +610,7 @@ func (Instance) Create(ctx context.Context, fields []resource.Field) (resource.R
 }
 
 type InstancesLogsCmd struct {
-	Name []string `arg:"" help:"Names of the instances to fetch logs for."`
+	Name []string `arg:"" predictor:"resource-key-instance" help:"Names of the instances to fetch logs for."`
 
 	Tail   int  `help:"Number of lines to show from the end of the logs."`
 	Follow bool `short:"f" help:"Follow log output."`
@@ -663,7 +663,7 @@ func (cmd *InstancesLogsCmd) Run(ctx context.Context, cfg *config.Config) error 
 }
 
 type InstancesStartCmd struct {
-	Name []string `arg:"" help:"Names of the instances to start."`
+	Name []string `arg:"" predictor:"resource-key-instance" help:"Names of the instances to start."`
 }
 
 func (cmd *InstancesStartCmd) Run(ctx context.Context) error {
@@ -695,7 +695,7 @@ func (cmd *InstancesStartCmd) Run(ctx context.Context) error {
 }
 
 type InstancesStopCmd struct {
-	Name []string `arg:"" help:"Names of the instances to stop."`
+	Name []string `arg:"" predictor:"resource-key-instance" help:"Names of the instances to stop."`
 
 	StopOpts
 }
@@ -733,7 +733,7 @@ func (cmd *InstancesStopCmd) Run(ctx context.Context) error {
 }
 
 type InstancesRestartCmd struct {
-	Name []string `arg:"" help:"Names of the instances to restart."`
+	Name []string `arg:"" predictor:"resource-key-instance" help:"Names of the instances to restart."`
 
 	StopOpts
 }
