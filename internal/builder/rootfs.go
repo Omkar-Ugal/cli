56ea9e02c8268ef93aa62cae866eae26ad380c12
Cezar Craciunoiu <cezar@unikraft.io>
2026-05-07 17:28:47 +0300

    feat(builder): Add erofs feature flag check
    Signed-off-by: Cezar Craciunoiu <cezar@unikraft.io>

--- internal/builder/rootfs.go
+++ internal/builder/rootfs.go
@@ -150,6 +150,12 @@ func BuildRootfs(ctx context.Context, opts BuildOpts) (_ []*imagespec.Image, rer
 		return nil, fmt.Errorf("at least one platform must be specified")
 	}
 
+	if opts.Rootfs.Format == "" &&
+		opts.Rootfs.Type != kraftfile.SourceTypeCpio &&
+		opts.Rootfs.Type != kraftfile.SourceTypeErofs {
+		opts.Rootfs.Format = DefaultRootfsFormat(opts.Platform)
+	}
+
 	switch opts.Rootfs.Type {
 	case kraftfile.SourceTypeCpio, kraftfile.SourceTypeErofs:
 		expected := map[kraftfile.SourceType]kraftfile.FsType{
