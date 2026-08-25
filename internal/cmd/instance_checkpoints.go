194fd755733b40440a355cec3b6a61253df25c9b
Justin Chadwell <justin@unikraft.com>
2026-08-25 11:26:09 +0100

    fix(instance-checkpoints): Show delete-lock at long verbosity
    It was hidden here but long on instance templates, for no reason other
    than oversight - the platform populates it identically for both.
    
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- internal/cmd/instance_checkpoints.go
+++ internal/cmd/instance_checkpoints.go
@@ -81,7 +81,7 @@ type InstanceCheckpoint struct {
 	UUID      string          `mirror:"instance.uuid" field:",long"`
 
 	Tags       []string `mirror:"instance.tags" field:",long" edit:"set,add,del"`
-	DeleteLock bool     `mirror:"instance.delete_lock" field:"delete-lock,hidden" edit:"set"`
+	DeleteLock bool     `mirror:"instance.delete_lock" field:"delete-lock,long" edit:"set"`
 
 	State types.InstanceState             `mirror:"instance.state" field:",short"`
 	Image types.ImageRef[reference.Named] `mirror:"instance.image" field:",short"`
